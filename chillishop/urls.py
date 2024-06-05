904ace5ad9800282d3590d18c47064e4c7910ef4
EL96cpp <el96cpp@gmail.com>
2024-06-05 10:12:54 +0300

    Added .html pages, added views.py and urls.py contents to make home page work

--- chillishop/urls.py
+++ chillishop/urls.py
@@ -14,9 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
+from chillishop import settings
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from shop.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include("shop.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseNotFound, Http404 
+from .models import *
+
 
 # Create your views here.
+def index(request):
+    return render(request, 'shop/index.html')
+
+def login(request):
+    return render(request, 'shop/login.html')
