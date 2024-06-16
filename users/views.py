b05e42d4b43c33733ba60701c79c8bd0cc37ac95
EL96cpp <el96cpp@gmail.com>
2024-06-16 16:04:43 +0300

    Added logout button and user info to the profile page

--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.views import LoginView
 from django.views.generic import CreateView
 from django.http import HttpResponseRedirect
 from django.contrib.auth.forms import AuthenticationForm
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 
 from users.forms import *
 from users.models import *
@@ -14,7 +14,7 @@ class RegisterUser(CreateView):
     form_class = RegisterUserForm
     model = User
     template_name = 'users/register.html'
-    success_url = 'home'
+    success_url = reverse_lazy("home") 
 
     def get_context_data(self, **kwargs):
         return super().get_context_data(**kwargs)
@@ -24,11 +24,20 @@ class LoginUser(LoginView):
     form_class = LoginUserForm
     model = User
     template_name = 'users/login.html'
-    success_url = 'users/profile.html'
 
     def get_context_data(self, **kwargs):
         return super().get_context_data(**kwargs)
 
 
+    def get_success_url(self):
+        return reverse_lazy("users:profile")
+
+
 def profile(request):
     return render(request, 'users/profile.html')
+
+
+def logout_user(request):
+    auth.logout(request)
+    print("Logout call!!!")
+    return redirect('home')
