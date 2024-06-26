9d8600a5acaa1ec5e57e7cb440121c8db63731fa
EL96cpp <el96cpp@gmail.com>
2024-06-26 13:03:37 +0300

    Added removing products from cart functionality

--- carts/views.py
+++ carts/views.py
@@ -45,5 +45,26 @@ def cart_change(request):
 
 
 def cart_remove(request):
-    pass
+    
+    cart_id = request.POST.get("cart_id")
+    print("keker!")
+    print(cart_id)
+
+    cart = Cart.objects.get(product=cart_id)
+    quantity = cart.quantity
+    cart.delete()
+
+    print(quantity, " quantity")
+
+    user_cart = get_user_carts(request)
+    cart_items_html = render_to_string("carts/includes/cart.html", {"carts": user_cart}, request=request)
+
+    response_data = {
+        "message": "Товар удалён",
+        "cart_items_html": cart_items_html,
+        "quantity_deleted": quantity        
+    }
+
+
+    return JsonResponse(response_data)
 
--- shop/static/shop/js/jquery-ajax.js
+++ shop/static/shop/js/jquery-ajax.js
@@ -3,15 +3,10 @@ $(document).ready(function () {
 
     $(document).on("click", ".card_button", function(e) {
 
-        console.log("Click!");
-
         e.preventDefault();
 
         var product_id = $(this).data("product-id");
         var add_to_cart_url = $(this).attr("href");
-
-        console.log(product_id, add_to_cart_url);
-
 
         $.ajax({
             
@@ -29,6 +24,46 @@ $(document).ready(function () {
             
         });
         
+
+    });
+
+    $(document).on("click", ".remove_from_cart", function (e) {
+
+        console.log("Click remove!");
+
+        e.preventDefault();
+
+        var goods_in_cart_count = $("#cart_total_quantity");
+        var cart_count = parseInt(goods_in_cart_count.text() || 0);
+
+        var cart_id = $(this).data("cart-id");
+        var remove_url = $(this).attr("href");
+
+        console.log(cart_id, remove_url);
+        console.log(cart_count);
+
+        $.ajax({
+            
+            type: "POST",
+            url: remove_url,
+            data: {
+                cart_id: cart_id,
+                csrfmiddlewaretoken: $("[name=csrfmiddlewaretoken]").val(),
+            },
+
+            success: function (data) {
+                var cartItemsContainer = $("#cart-items-container");
+                cartItemsContainer.html(data.cart_items_html);
+
+                cart_count -= data.quantity_deleted;
+                goods_in_cart_count.text(cart_count);
+
+                var cart_items_container = $("#cart-items-container");
+                cart_items_container.html(data.cart_items_html);
+
+            }
+            
+        });
 
     });
 
